1339866236a05d5a0adb05e96fe657305dbb15ae
maria <mo394632@students.mimuw.edu.pl>
2019-03-03 22:29:28 +0100

    added first task assembler

--- minix_source/sync.sh
+++ minix_source/sync.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")" # set current working directory to the directory of the script
 
-rsync -uav --exclude=".git" ./usr/include/ minix:/usr/include/
-rsync -uav --exclude=".git" ./usr/src/ minix:/usr/src/
+rsync -uav --exclude=".git" ./usr/include/ so:/usr/include/
+rsync -uav --exclude=".git" ./usr/src/ so:/usr/src/
