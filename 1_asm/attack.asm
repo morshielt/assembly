0072f9ed7f2ed9e490643afe15b331d5be7b34ff
maria <mo394632@students.mimuw.edu.pl>
2019-03-08 00:15:56 +0100

    arg number + file opening

--- 1_asm/attack.asm
+++ 1_asm/attack.asm
@@ -1,15 +1,39 @@
+section .data
+    msg db 'hello, world', 10
+    msg_len equ $ - msg
+
 section .text
-global _start
+  global _start
 
 _start:
-    pop rdi ; argc
-    pop rdi ; argv[0]
+    mov rbp,rsp
+    mov rcx, [rbp] ; argc
+    dec rcx ; pierwszy arg się nie liczy
+    cmp rcx, 1 ; czy podane dokł. 1 arg.
+    je _args_ok
+    jmp _failure
 
+_args_ok:
+    ;open(const char *pathname, int flags, mode_t mode);
+    mov rax, 2 ; sys_open
+    lea rdi, [rbp+16] ; pathaddress
+    mov rdi, [rdi] ;pathname
+    mov rsi, 0 ; O_RDONLY
+    mov rdx, 0 ; NO MODE
+    syscall
+    cmp rax, 0
+    jge _file_opened
+    jmp _failure
 
-_exit:
+_file_opened:
+   jmp _success
+
+_success:
     mov rax, 60
     mov rdi, 0
     syscall
 
-
-
+_failure:
+    mov rax, 60
+    mov rdi, 1
+    syscall
--- 1_asm/compile.sh
+++ 1_asm/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nasm -f elf64 -o attack.o attack.asm
 ld --fatal-warnings -o attack attack.o
-./attack
+./attack _test_1.0
