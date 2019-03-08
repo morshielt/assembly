1691cf49b87c34016830af55f252769252f4a8ef
maria <mo394632@students.mimuw.edu.pl>
2019-03-08 15:37:35 +0100

    first assebler lab + attack assgn update

---
+++ lab/lab2/lab.asm
@@ -0,0 +1,10 @@
+;1.5
+
+global mac ; etykietę trzeba dać jako global, żeby była widoczna na zewnątrz mofułu asembelrowego
+
+section .text
+
+mac:
+
+;TODO zadanie A4
+;połączenie z C, przestrzeganie tego czyszczenia po funkcji
