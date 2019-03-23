9bf13acb42949572dd9ebada660c71b4a614fdd7
maria <mo394632@students.mimuw.edu.pl>
2019-03-23 19:41:32 +0100

    synchronize, jump table, no_pie all done; TODO calling functions (registers and stack)

--- assembler/lab/2/hw.asm
+++ assembler/lab/2/hw.asm
@@ -1,5 +1,10 @@
+%macro jump_table_init 2 ; calculate ((%1) + (%2) * N) * 8, meaning index '[%1][%2]'
+    mov rax, %1
+    mov [jump_table + %2*8], rax
+%endmacro
+
 section .bss
-    jump_table resb 128
+    jump_table resq 128
 
 section .text
 global _start
@@ -16,17 +21,18 @@ _start:
     syscall
 
     ; jump_table init
-    mov rax, label1
-    mov [jump_table + '1'*8], rax
+    jump_table_init label1, 125
+;    mov rax, label1
+;    mov [jump_table + 125*8], rax
 
     mov rax, label2
-    mov [jump_table + '2'*8], rax
+    mov [jump_table + 127*8], rax
 
     mov rax, _exit4
-    mov [jump_table + '3'*8], rax
+    mov [jump_table + 126*8], rax
     ; end of init
 
-    jmp [jump_table + '1'*8] ; label1
+    jmp [jump_table + 125*8] ; label1
 
 _exit:
     mov rax, 60
@@ -34,7 +40,7 @@ _exit:
     syscall
 
 label1:
-    jmp [jump_table + '2'*8] ;label2
+    jmp [jump_table + 127*8] ;label2
 
 _exit4:
     mov rax, 60
@@ -42,6 +48,6 @@ _exit4:
     syscall
 
 label2:
-    jmp [jump_table + '3'*8] ; _exit4
+    jmp [jump_table + 126*8] ; _exit4
 
 
